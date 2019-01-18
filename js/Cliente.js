6f8bf8f428df040998c1e791e0ba346678540f2e
Skr <oscaraparisisancho@gmail.com>
2019-01-18 03:58:21 +0100

    Ficha muestra los datos del cliente seleccionado

--- js/Cliente.js
+++ js/Cliente.js
@@ -1,7 +1,7 @@
 'use strict';
 class Cliente {
 
-    constructor(tipo,nombre,correo,telefono,ciudad,edad,sexo,fechaLlegada){
+    constructor(tipo,nombre,correo,telefono,ciudad,edad,sexo,fechaLlegada,comentario){
         this.tipo=tipo;
         this.nombre=nombre;
         this.correo=correo;
@@ -10,6 +10,7 @@ class Cliente {
         this.edad = edad;
         this.sexo = sexo;
         this.fechaLlegada = fechaLlegada;
+        this.comentario = comentario;
         
     }
     
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 //Array con todos los objetos cliente
 var clientesCartera = [];
+var cliente;
 
 
 
@@ -17,7 +18,22 @@ function refresh(){
 }
 
 function nuevoCliente(){
-    let a = new Cliente("regular","Skr","correo@web.com", 666666666,"sin ciudad","sin sexo","Hoy");
+    let a = new Cliente("regular","sin nombre","correo@web.com", 666666666,"sin ciudad",99,"sin sexo","Hoy","sin comentarios");
     clientesCartera.push(a);
     refresh();
+    cliente = a;
+    cambiarVistaCliente();
+}
+
+function cambiarVistaCliente(){
+    document.getElementById("cliente-nombre").innerText = cliente.nombre;
+    document.getElementById("cliente-fecha").innerText = cliente.fechaLlegada;
+    document.getElementById("cliente-telefono").placeholder = cliente.telefono;
+    document.getElementById("cliente-ciudad").placeholder = cliente.ciudad;
+    document.getElementById("cliente-edad").placeholder = cliente.edad;
+    document.getElementById("cliente-correo").placeholder = cliente.correo;
+    document.getElementById("cliente-sexo").placeholder = cliente.sexo;
+    document.getElementById("cliente-tipo").placeholder = cliente.tipo;
+    document.getElementById("cliente-comentario").placeholder = cliente.comentario;
+  
 }
